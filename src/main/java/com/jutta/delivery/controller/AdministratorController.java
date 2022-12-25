7cfe0bb9d6d095c29e8267a22976bb8f20783f60
Jutta <yangzigui99@gmail.com>
2022-12-25 23:56:26 +0800

    25.12.2022 commit

--- src/main/java/com/jutta/delivery/controller/AdministratorController.java
+++ src/main/java/com/jutta/delivery/controller/AdministratorController.java
@@ -20,8 +20,17 @@ public class AdministratorController {
     @Autowired
     private AdministratorService administratorService;
 
+    // 员工登录
+
     @PostMapping("/login")
     public R<Administrator> login(HttpServletRequest request, @RequestBody Administrator administrator) {
+       /* 1、将页面提交的密码password进行md5加密处理
+        2、根据页面提交的用户名username查询数据库
+        3、如果没有查询到则返回登录失败结果
+        4、密码比对，如果不一致则返回登录失败结果
+        5、查看员工状态，如果为已禁用状态，则返回员工已禁用结果
+        6、登录成功，将员工id存入Session并返回登录成功结果*/
+
         return null;
     }
 }
