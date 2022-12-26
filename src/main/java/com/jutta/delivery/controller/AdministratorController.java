64620a406fe55799aa98b9bea82f0a89b2e212e4
Jutta <yangzigui99@gmail.com>
2022-12-26 23:39:28 +0800

    26.12.2022 commit

--- src/main/java/com/jutta/delivery/controller/AdministratorController.java
+++ src/main/java/com/jutta/delivery/controller/AdministratorController.java
@@ -1,10 +1,12 @@
 package com.jutta.delivery.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.jutta.delivery.common.R;
 import com.jutta.delivery.entity.Administrator;
 import com.jutta.delivery.service.AdministratorService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,13 +26,33 @@ public class AdministratorController {
 
     @PostMapping("/login")
     public R<Administrator> login(HttpServletRequest request, @RequestBody Administrator administrator) {
-       /* 1、将页面提交的密码password进行md5加密处理
-        2、根据页面提交的用户名username查询数据库
-        3、如果没有查询到则返回登录失败结果
-        4、密码比对，如果不一致则返回登录失败结果
-        5、查看员工状态，如果为已禁用状态，则返回员工已禁用结果
-        6、登录成功，将员工id存入Session并返回登录成功结果*/
+//        1、将页面提交的密码password进行md5加密处理
+        String password = administrator.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
 
-        return null;
+//        2、根据页面提交的用户名username查询数据库
+        LambdaQueryWrapper<Administrator> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Administrator::getUsername, administrator.getUsername());
+        Administrator adm = administratorService.getOne(queryWrapper);
+
+//        3、如果没有查询到则返回登录失败结果
+        if (adm == null) {
+            return R.error("Login failed");
+        }
+
+//        4、密码比对，如果不一致则返回登录失败结果
+        if (!adm.getPassword().equals(password)) {
+            return R.error("Login failed");
+        }
+
+//        5、查看管理状态，如果为已禁用状态，则返回管理已禁用结果
+        if (adm.getStatus() == 0) {
+            return R.error("The account is disabled");
+        }
+
+//        6、登录成功，将管理id存入Session并返回登录成功结果
+        request.getSession().setAttribute("administrator", adm.getId());
+        return R.success(adm);
+
     }
 }
